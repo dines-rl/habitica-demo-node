45e0389f36c70c95e676666ce074f739264fda10
Alexander Dines <160077924+dines-rl@users.noreply.github.com>
2024-07-17 12:11:24 -0700

    cp dines

--- runloop/devboxTest.ts
+++ runloop/devboxTest.ts
@@ -6,11 +6,18 @@ export const useDevbox = RLFunc({
   id: "useDevbox",
   run: async function (request, { systemCoordinator, logger }) {
     logger.info(`TOKEN: ${gh_token}`);
+    // write code to log counting up every second for 1000 seconds
+    let count = 0;
+    while (count++ < 1000) {
+      logger.info("Hello World " + count);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+
     //let devbox = await systemCoordinator.createDevbox(undefined, undefined, [
     //  "curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -",
     //  "sudo apt install -y --no-install-recommends nodejs libkrb5-dev",
     //]);
-    logger.info("Devbox created: ", devbox.id);
+    logger.info("Devbox created: ");
 
     //await devbox.execTool.exec(
     //  `GH_TOKEN=${gh_token} gh repo clone HabitRPG/habitica ./code -- --depth=1`
@@ -28,6 +35,6 @@ export const useDevbox = RLFunc({
     //let result = await devbox.execTool.exec("cd code && npm run lint");
     logger.info(`Lint result${result.stdout}`);
 
-    return { "readresult:": readResult, lintResult: result.stdout };
+    return { Hello: "World" };
   },
 });
