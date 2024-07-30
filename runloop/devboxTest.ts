3d756a7e9ae37a85e8d010b84c36bf3634f78ef6
Alexander Dines <160077924+dines-rl@users.noreply.github.com>
2024-07-30 14:37:33 -0700

    cp dines

--- runloop/devboxTest.ts
+++ runloop/devboxTest.ts
@@ -14,6 +14,7 @@ export const useDevbox = RLFunc({
     ((devbox.execTool as any).sshClient as any).ssh.connection?.setMaxListeners(
       200
     );
+    throw new Error("Test Error");
     await devbox.fileTool.writeFile("test.txt", "Hello World");
     for (let i = 0; i < num; i++) {
       await devbox.fileTool.readFile("test.txt");
